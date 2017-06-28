6e2de8d9970a772255545eecf13ec55157491049
Z1wolfmaster1 <suiciniv2016@outlook.com>
2017-06-28 10:29:51 -0300

    Update Conversor de dias.c

--- Conversor de dias.c
+++ Conversor de dias.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <string.h>
 
 int main() {
-	setlocale(LC_ALL,"");
 	long int x, a=0, m=0, d=0;
 	
 	printf("Digite uma quantidade de dias: ");
