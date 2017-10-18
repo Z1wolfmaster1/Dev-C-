aaf3a41ffcb0540277309e4e059d56edced4d802
Z1wolfmaster1 <suiciniv2016@outlook.com>
2017-10-18 20:48:25 -0200

    Add files via upload

---
+++ Joojo da velha vf.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"portuguese");
+	int i, j, x, y, pv[3][3];
+	char jooj1[100], jooj2[100], p[3][3]; 
+	
+	//printf("X|X|X\nO|O|O\nX|X|X\n");
+	printf("Jogador 1[X]: ");
+	scanf("%[^\n]s", &jooj1);
+	fflush(stdin);
+	printf("Jogador 2[O]: ");
+	scanf("%[^\n]s", &jooj2);
+	
+	for(i=0; i<3; i++)
+	for(j=0; j<3; j++){
+		p[i][j]=' ';
+		pv[i][j]=0;
+	}
+	
+	do{
+		jooj1:
+	 	system("cls");
+		printf("  1 2 3\n1 %c|%c|%c\n2 %c|%c|%c \n3 %c|%c|%c \n\n%s: ", p[0][0],p[0][1],p[0][2],p[1][0],p[1][1],p[1][2],p[2][0],p[2][1],p[2][2],jooj1);
+		scanf("%d%d", &x, &y);
+		x-=1;
+		y-=1;
+		if(pv[x][y]==0){
+			pv[x][y]=1;
+			p[x][y]='X';
+		}
+		
+		else{
+			printf("Não pode joojar nessa posição!\n\n");
+			system("pause");
+			goto jooj1;
+		}
+		
+		if(p[0][0]=='X'&&p[0][1]=='X'&&p[0][2]=='X'||p[0][2]=='X'&&p[1][1]=='X'&&p[2][0]=='X'||p[0][0]=='X'&&p[1][0]=='X'&&p[2][0]=='X'||p[0][1]=='X'&&p[1][1]=='X'&&p[2][1]=='X'||p[0][3]=='X'&&p[1][3]=='X'&&p[2][3]=='X'||p[0][0]=='X'&&p[0][1]=='X'&&p[0][2]=='X'||p[1][0]=='X'&&p[1][1]=='X'&&p[1][2]=='X'||p[2][0]=='X'&&p[2][1]=='X'&&p[2][2]=='X'){
+			system("cls");
+			printf("  1 2 3\n1 %c|%c|%c\n2 %c|%c|%c \n3 %c|%c|%c \n\n", p[0][0],p[0][1],p[0][2],p[1][0],p[1][1],p[1][2],p[2][0],p[2][1],p[2][2],jooj1);
+			printf("%s venceu!", jooj1);
+			break;
+		}
+		
+		jooj2:
+		system("cls");
+		printf("  1 2 3\n1 %c|%c|%c\n2 %c|%c|%c \n3 %c|%c|%c \n\n%s: ", p[0][0],p[0][1],p[0][2],p[1][0],p[1][1],p[1][2],p[2][0],p[2][1],p[2][2],jooj2);
+		scanf("%d%d", &x, &y);
+		x-=1;
+		y-=1;
+		if(pv[x][y]==0){
+			pv[x][y]=1;
+			p[x][y]='O';
+		}
+		
+		else{
+			printf("Não pode joojar nessa posição!\n\n");
+			system("pause");
+			goto jooj2;
+		}
+		
+		if(p[0][0]=='O'&&p[0][1]=='O'&&p[0][2]=='O'||p[0][2]=='O'&&p[1][1]=='O'&&p[2][0]=='O'||p[0][0]=='O'&&p[1][0]=='O'&&p[2][0]=='O'||p[0][1]=='O'&&p[1][1]=='O'&&p[2][1]=='O'||p[0][3]=='O'&&p[1][3]=='O'&&p[2][3]=='O'||p[0][0]=='O'&&p[0][1]=='O'&&p[0][2]=='O'||p[1][0]=='O'&&p[1][1]=='O'&&p[1][2]=='O'||p[2][0]=='O'&&p[2][1]=='O'&&p[2][2]=='O'){
+			system("cls");
+			printf("  1 2 3\n1 %c|%c|%c\n2 %c|%c|%c \n3 %c|%c|%c \n\n", p[0][0],p[0][1],p[0][2],p[1][0],p[1][1],p[1][2],p[2][0],p[2][1],p[2][2],jooj1);
+			printf("%s venceu!", jooj2);
+			break;
+		}
+		
+	} while(1);
+}
